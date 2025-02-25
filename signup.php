aaec9d704703241e8ae5b0b8ac325763ebdf2626
Sorabh Sharma <sorabhsharma9720@gmail.com>
2025-02-25 21:20:34 +0530

    files added

--- signup.php
+++ signup.php
@@ -18,6 +18,7 @@ else{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./src/output.css">
 </head>
 <body>
 <!-- php code -->
